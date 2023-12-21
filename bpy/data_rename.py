1333f29238acccc6e64cb76f3ff856abf0427001
fistnine <fistnine@outlook.com>
2023-12-21 18:51:55 +0800

    Update data_rename.py

--- bpy/data_rename.py
+++ bpy/data_rename.py
@@ -1,10 +1,12 @@
 import bpy
 import re
 
-_type='all_images'
+_type='all_images_autorename'
     #'action' #Rename action path.
     #'all_actions' #Rename all action channels name.
     #'all_images' #Rename all images file path.
+    #'all_images_autorename' #Images name = file path.
+
 _name='idel'
 _str="mixamorig:"
 _rep=""
@@ -25,7 +27,11 @@ def fn_rename_images_filepath():
     for i in bpy.data.images:
         if re.findall(r''+_str+'',i.filepath):
             i.filepath = i.filepath.replace(_str,_rep)
-            
+
+def fn_all_images_autorename():
+    for i in bpy.data.images:
+        i.name=i.filepath.replace('/','_')
+
 match _type:
     case 'action':
         fn_rename_action(_name)
@@ -34,3 +40,5 @@ match _type:
             fn_rename_action(_action.name)
     case 'all_images':
         fn_rename_images_filepath()
+    case 'all_images_autorename':
+        fn_all_images_autorename()
